f66bfc0824f4d274a7e51a6af3b8bb9550408758
ikeniborn <ikeniborn@gmail.com>
2020-01-18 00:19:48 +0300

    update

--- Google/Function/getLastDateArray.js
+++ Google/Function/getLastDateArray.js
@@ -1,11 +1,14 @@
-// get sheet with name Trello, clear all contents, add titles
 function getLastDateArray(sheetID, sheetName) {
   var ss = SpreadsheetApp.openById(sheetID).getSheetByName(sheetName)
   var array = ss.getDataRange().getValues()
   var lastColumn = ss.getLastColumn() - 1
-  // get last date from loaf array
+  // get last date from load array
   var filterArray = array.filter(function (row) {
-    return row[lastColumn] == sheetName
+    if (sheetName == 'Факт' || sheetName == 'Бюджет') {
+      return row[lastColumn] == sheetName
+    } else {
+      return row
+    }
   })
   var maxDate = filterArray.reduce(function (a, b) {
     return a[0] > b[0] ? a[0] : b[0]
