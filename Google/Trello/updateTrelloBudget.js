e6c67df9dab8416cac81a005aa65f1558d289dcf
ikeniborn <ikeniborn@gmail.com>
2020-01-21 22:44:31 +0300

    добавление вебхука бюджета и копирование в учет

--- Google/Trello/updateTrelloBudget.js
+++ Google/Trello/updateTrelloBudget.js
@@ -1,72 +1,28 @@
-// trello variables
-var apiKey = '9dae7dd3ce328d61e67edb4557149502'
-var apiToken = getTokenTrello()
-var apiRoot = 'https://api.trello.com/1/'
-var boardId = '5e205c1e08b1ce8bac5a28e6'
-var sourceSheetID = '10cO9hdYF-K4cLMC7ZbrDqM0RByswKAFfd3E3ggwyl8E'
-var sourceSheetName = 'trelloBudget'
-var metadataSheetName = 'period'
-var targetSheetID = '1mBsaVLbKLoIXN2WY9Oi-XBPbViwbCt29gozLkOL5sLc'
-var targetSheetName = 'Бюджет'
-// getMetadata(sourceSheetID, sourceSheetName) получение данных по периодам.
-var budgetPeriodNow = getMetadata(sourceSheetID, metadataSheetName)[0][1]
-var budgetPeriodPrev = getMetadata(sourceSheetID, metadataSheetName)[1][1]
-var revenueDayIlya = getMetadata(sourceSheetID, metadataSheetName)[0][2]
-var revenueDayOksana = getMetadata(sourceSheetID, metadataSheetName)[0][3]
-//  getDirItem() получение справочника статей
-var dirItem = getDirItem() // получение справочника статей
-var currDate = new Date().getDate()
-
-function updateTrelloBudget() {
-
+function updateTrelloBudget(postData) {
   // get sheet Google
-  var ss = SpreadsheetApp.openById(sourceSheetID).getSheetByName(sourceSheetName)
+  var ss = SpreadsheetApp.openById(sourceSheetID).getSheetByName(sourceSheetNameBudget)
+  // get last date from source array
 
-  // get last data from board. Function return array with next attribute [commentDate, userName, listName, nomenclatureName, sumData, comment]
-  // loadFromTrello(apiKey, apiToken, apiRoot, boardId, sourceSheetID, sourceSheetName)
-  var lastDataFromTrello = loadFromTrello(apiKey, apiToken, apiRoot, boardId, sourceSheetID, sourceSheetName)
+  var commentDate = new Date(postData.action.date)
+  var userName = postData.action.memberCreator.username
+  var listName = postData.action.data.list.name
+  var nomenclatureName = postData.action.data.card.name
+  var actionDataText = postData.action.data.text
+  var sumData = actionDataText.match(/^\d+/)
+  var commentData = actionDataText.split(sumData).join('').replace(/^[.,\,, ,\-,\/,\\]/, ' ').trim()
+  var period = budgetPeriodNow
+  var dataDirItem = accountingItem.filter(function (row) {
+    return row[1] == nomenclatureName
+  })
+  var billName = dataDirItem[0][3]
+  var itemName = dataDirItem[0][2]
 
-  for (var i = 0; i < lastDataFromTrello.length; i++) {
-    var commentDate = new Date(lastDataFromTrello[i][0])
-    var userName = lastDataFromTrello[i][1]
-    var listName = lastDataFromTrello[i][2]
-    var nomenclatureName = lastDataFromTrello[i][3]
-    var sumData = lastDataFromTrello[i][4]
-    var commentData = lastDataFromTrello[i][5]
-    var factPeriod = []
-    if (budgetPeriodPrev.length == 0) {
-      factPeriod = budgetPeriodNow
-    } else {
-      if (listName !== 'Оксана') {
-        if (currDate >= revenueDayIlya) {
-          factPeriod = budgetPeriodNow
-        } else {
-          factPeriod = budgetPeriodPrev
-        }
-      } else {
-        if (currDate >= revenueDayOksana) {
-          factPeriod = budgetPeriodNow
-        } else {
-          factPeriod = budgetPeriodPrev
-        }
-      }
-    }
-    var dataDirItem = dirItem.filter(function (row) {
-      return row[0] == nomenclatureName
-    })
-    var billName = dataDirItem[0][2]
-    var itemName = dataDirItem[0][1]
-
-    ss.appendRow([commentDate, factPeriod, listName, billName, itemName, nomenclatureName, sumData, commentData, userName])
-
-  }
+  ss.appendRow([commentDate, period, listName, listName, billName, itemName, nomenclatureName, +sumData, commentData, userName])
   // Удаление пустых строк
   var maxRows = ss.getMaxRows()
   var lastRow = ss.getLastRow()
   if (maxRows - lastRow != 0) {
     ss.deleteRows(lastRow + 1, maxRows - lastRow)
   }
-
-  // copyData(sourceSheetID, targetSheetID, sourceSheetName, targetSheetName) копирование данных из истоничка в учет
-  copyData(sourceSheetID, targetSheetID, sourceSheetName, targetSheetName)
+  return true
 }
--- Google/Trello/updateTrelloFact.js
+++ Google/Trello/updateTrelloFact.js
@@ -1,54 +1,49 @@
 function updateTrelloFact(postData) {
   // get sheet Google
-  var ss = SpreadsheetApp.openById(sourceSheetID).getSheetByName(sourceSheetName)
+  var ss = SpreadsheetApp.openById(sourceSheetID).getSheetByName(sourceSheetNameFact)
   // get last date from source array
-
-  if (new Date(postData.action.date) > new Date(maxDate.getTime()) && postData.action.type == 'commentCard') {
-    var commentDate = new Date(postData.action.date)
-    var userName = postData.action.memberCreator.username
-    var listName = postData.action.data.list.name
-    var nomenclatureName = postData.action.data.card.name
-    var actionDataText = postData.action.data.text
-    var sumData = actionDataText.match(/^\d+/)
-    var commentData = actionDataText.split(sumData).join('').replace(/^[.,\,, ,\-,\/,\\]/, ' ').trim()
-    var factPeriod = []
-    if (factPeriodPrev.length == 0) {
-      factPeriod = factPeriodNow
-    } else {
-      if (listName !== 'Оксана') {
-        if (currDate >= revenueDayIlya) {
-          factPeriod = factPeriodNow
-        } else {
-          factPeriod = factPeriodPrev
-        }
+  var commentDate = new Date(postData.action.date)
+  var userName = postData.action.memberCreator.username
+  var listName = postData.action.data.list.name
+  var nomenclatureName = postData.action.data.card.name
+  var actionDataText = postData.action.data.text
+  var sumData = actionDataText.match(/^\d+/)
+  var commentData = actionDataText.split(sumData).join('').replace(/^[.,\,, ,\-,\/,\\]/, ' ').trim()
+  var period = []
+  if (factPeriodPrev.length == 0) {
+    period = factPeriodNow
+  } else {
+    if (listName !== 'Оксана') {
+      if (currDate >= revenueDayIlya) {
+        period = factPeriodNow
       } else {
-        if (currDate >= revenueDayOksana) {
-          factPeriod = factPeriodNow
-        } else {
-          factPeriod = factPeriodPrev
-        }
+        period = factPeriodPrev
+      }
+    } else {
+      if (currDate >= revenueDayOksana) {
+        period = factPeriodNow
+      } else {
+        period = factPeriodPrev
       }
     }
-    var dataDirItem = accountingItem.filter(function (row) {
-      return row[1] == nomenclatureName
-    })
-    var billName = dataDirItem[0][3]
-    var itemName = dataDirItem[0][2]
-
-    ss.appendRow([commentDate, factPeriod, listName, listName, billName, itemName, nomenclatureName, +sumData, commentData, userName])
-    // Обновление даты зарплаты
-    if (itemName == 'Зарплата') {
-      // update date for budget period. Start with next parametr (sheetID, SheetName, commentDate, listName)
-      updateRevenueDate(sourceSheetID, periodSheetName, commentDate, listName)
-    }
-    var actionId = postData.action.id
-    addReaction(apiRoot, apiToken, apiKey, boardId, actionId)
-    // Удаление пустых строк
-    var maxRows = ss.getMaxRows()
-    var lastRow = ss.getLastRow()
-    if (maxRows - lastRow != 0) {
-      ss.deleteRows(lastRow + 1, maxRows - lastRow)
-    }
-    return true
   }
+  var dataDirItem = accountingItem.filter(function (row) {
+    return row[1] == nomenclatureName
+  })
+  var billName = dataDirItem[0][3]
+  var itemName = dataDirItem[0][2]
+
+  ss.appendRow([commentDate, period, listName, listName, billName, itemName, nomenclatureName, +sumData, commentData, userName])
+  // Обновление даты зарплаты
+  if (itemName == 'Зарплата') {
+    // обновление даты периода бюджета
+    updateRevenueDate(sourceSheetID, periodSheetName, commentDate, listName)
+  }
+  // Удаление пустых строк
+  var maxRows = ss.getMaxRows()
+  var lastRow = ss.getLastRow()
+  if (maxRows - lastRow != 0) {
+    ss.deleteRows(lastRow + 1, maxRows - lastRow)
+  }
+  return true
 }
