e822e0a82072d4195851862b472be65b86052337
ikeniborn <ikeniborn@gmail.com>
2020-01-21 23:24:42 +0300

    условие проверки запуска обработки события по факту и бюджету

--- Google/Function/doPost.js
+++ Google/Function/doPost.js
@@ -1,21 +1,30 @@
 function doPost(e) {
   var postData = JSON.parse(e.postData.contents)
-  var ss = SpreadsheetApp.openById('10cO9hdYF-K4cLMC7ZbrDqM0RByswKAFfd3E3ggwyl8E')
+  // отладка вебхука
+  var ss = SpreadsheetApp.openById(sourceSheetID)
   var sheet = ss.getSheetByName('test')
   sheet.getRange(1, 1).setValue(postData)
+  // проверка события и вебхука
   if (postData.action.type == 'commentCard') {
-    if (new Date(postData.action.date) > new Date(maxDateFact.getTime()) && postData.action.data.board.name == targetSheetNameFact) {
-      var postDataEvent = updateTrelloFact(postData)
-      if (postDataEvent) {
-        addReaction(apiRoot, apiToken, apiKey, postData.action.id)
-        copyData(sourceSheetID, targetSheetID, sourceSheetNameFact, targetSheetNameBudget)
+    if (postData.action.data.board.name == targetSheetNameFact) {
+      if (new Date(postData.action.date) > new Date(maxDateFact.getTime())) {
+        var postDataEvent = updateTrelloFact(postData)
+        if (postDataEvent) {
+          copyData(sourceSheetID, targetSheetID, sourceSheetNameFact, targetSheetNameFact)
+        }
       }
-    } else if (new Date(postData.action.date) > new Date(maxDateBudget.getTime()) && postData.action.data.board.name == targetSheetNameBudget) {
-      var postDataEvent = updateTrelloBudget(postData)
-      if (postDataEvent) {
-        addReaction(apiRoot, apiToken, apiKey, postData.action.id)
-        copyData(sourceSheetID, targetSheetID, sourceSheetNameBudget, targetSheetNameBudget)
+    } else if (postData.action.data.board.name == targetSheetNameBudget) {
+      if (new Date(postData.action.date) > new Date(maxDateBudget.getTime())) {
+        var postDataEvent = updateTrelloBudget(postData)
+        if (postDataEvent) {
+          copyData(sourceSheetID, targetSheetID, sourceSheetNameBudget, targetSheetNameBudget)
+        }
       }
+    }
+    if (postData.action.memberCreator.username == 'oksanatischenko') {
+      addReaction(apiRoot, apiToken, apiKey, postData.action.id, buuReaction)
+    } else {
+      addReaction(apiRoot, apiToken, apiKey, postData.action.id, jackdawReaction)
     }
   }
 }
