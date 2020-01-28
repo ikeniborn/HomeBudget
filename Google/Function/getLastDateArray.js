6ec006a5fc5b7caad6af18b30cf96e58af3fb8e4
ikeniborn <ikeniborn@gmail.com>
2020-01-28 22:23:33 +0300

    обновление функции получения максимальнй даты в массиве

--- Google/Function/getLastDateArray.js
+++ Google/Function/getLastDateArray.js
@@ -1,23 +1,17 @@
+// получение максимальной даты в массиве
 function getLastDateArray(sheetID, sheetName, filter) {
-  if (filter == undefined) filter = false
-  var ss = SpreadsheetApp.openById(sheetID).getSheetByName(sheetName)
-  var array = ss.getDataRange().getValues()
-  var lastColumn = ss.getLastColumn() - 1
-  // get last date from load array
-  var targetSheetName = ['Факт', 'Бюджет']
-  var arrayDate = []
-  for (i = 0; i < array.length; i++) {
-    var row = array[i]
-    if (targetSheetName.indexOf(sheetName) !== -1) {
-      if (row[lastColumn] == filter) {
-        arrayDate.push(row[0])
-      }
-    } else {
-      arrayDate.push(row[0])
+  var array = getCurrData(getAllData(sheetID, sheetName))
+  var arrayDate = array.reduce(function (search, all) {
+    if (all.sourceList == filter) {
+      search.push(all.date)
+    } else if (filter == undefined) {
+      search.push(all.date)
     }
-  }
+    return search
+  }, [])
+  var firstDate = startDate(90)
   var maxDate = arrayDate.reduce(function (a, b) {
     return a > b ? a : b
-  }, startDate(1))
+  }, firstDate)
   return new Date(maxDate.getTime() + 1000)
 }
