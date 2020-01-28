2a0fc59dd7411d20bf58f5277b6624a88a2244f5
ikeniborn <ikeniborn@gmail.com>
2020-01-28 15:57:29 +0300

    up

--- Google/Function/getTotalSum.js
+++ Google/Function/getTotalSum.js
@@ -1,12 +1,22 @@
 function getTotalSum(SheetID, SheetName, уmd, cfo, bill, account, nomenclature) {
+  /*
+    SheetID - идентификатор страницы гугл
+    SheetName - наименование листа страницы
+    уmd - период в формате yyyymmdd
+    cfo - наименование цфо
+    bill - наименование счета
+    account - наименование стать
+    nomenclature - наименование номенклатуры
+   */
+  // перепесать редьюс через вложенную функцию
   var ssData = SpreadsheetApp.openById(SheetID).getSheetByName(SheetName)
-  var allDataFact = ssData.getDataRange().getValues();
+  var allDataFact = ssData.getDataRange().getValues()
   var total = {}
   var totalBill = 0
   var totalAccount = 0
   var totalNomenclature = 0
   // получение суммы по счету
-  var filterDataBill = allDataFact.reduce(function (row, array) {
+  allDataFact.reduce(function (row, array) {
     var ymdRow = getYMD(array[1]).ymd
     if (ymdRow == уmd && array[2] == cfo && array[4] == bill) {
       totalBill += array[7]
