754a574bf1f6dc8526cc5d2edada486e02c7506a
ikeniborn <ikeniborn@gmail.com>
2020-01-28 20:53:29 +0300

    рабочая функция итоговых сумм

--- Google/Function/getTotalSum.js
+++ Google/Function/getTotalSum.js
@@ -1,54 +1,36 @@
 function getTotalSum(sheetID, sheetName, уmd, cfo, bill, account, nomenclature) {
   /*
-    SheetID - идентификатор страницы гугл
-    SheetName - наименование листа страницы
+    sheetID - идентификатор страницы гугл
+    sheetName - наименование листа страницы
     уmd - период в формате yyyymmdd
     cfo - наименование цфо
     bill - наименование счета
     account - наименование стать
     nomenclature - наименование номенклатуры
    */
-  // перепесать редьюс через вложенную функцию
   var allDataFact = filterDataFromArray(getAllData(sheetID, sheetName), уmd)
-  var billSum = 0
-  var accountSum = 0
-  var nomenclatureSum = 0
-  var reducers = {
-    bill: function (state, item) {
-      if (item.cfo == cfo && item.bill == bill) {
-        billSum += item.sum
-      }
-      return billSum
-    },
-    account: function (state, item) {
-      if (item.cfo == cfo && item.bill == bill && item.account == account) {
-        accountSum += item.sum
-      }
-      return accountSum
-    },
-    nomenclature: function (state, item) {
-      if (item.cfo == cfo && item.bill == bill && item.account == account && item.nomenclature == nomenclature) {
-        nomenclatureSum += item.sum
-      }
-      return nomenclatureSum
+  var total = {}
+
+  total.bill = allDataFact.reduce(function (sum, array) {
+    if (array.cfo == cfo && array.bill == bill) {
+      sum += array.sum
     }
-  }
+    return sum
+  }, 0)
 
-  var combineReducers = function (reducers) {
-    return function (state, item) {
-      return Object.keys(reducers).reduce(function (nextState, key) {
-        return reducers[key](state, item)
-      }, {})
+  total.account = allDataFact.reduce(function (sum, array) {
+    if (array.cfo == cfo && array.bill == bill && array.account == account) {
+      sum += array.sum
     }
-  };
+    return sum
+  }, 0)
 
-  var totalReducer = combineReducers(reducers);
-
-  var total = allDataFact.reduce(totalReducer, {
-    billSum: 0,
-    accountSum: 0,
-    nomenclatureSum: 0
-  })
+  total.nomenclature = allDataFact.reduce(function (sum, array) {
+    if (array.cfo == cfo && array.bill == bill && array.account == account && array.nomenclature == nomenclature) {
+      sum += array.sum
+    }
+    return sum
+  }, 0)
 
   return total
 }
