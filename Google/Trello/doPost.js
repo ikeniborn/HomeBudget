9552c8d6da5b84f8e09676cdf784f84babde0d00
ikeniborn <ikeniborn@gmail.com>
2020-01-30 23:19:26 +0300

    up

--- Google/Trello/doPost.js
+++ Google/Trello/doPost.js
@@ -4,30 +4,30 @@ function doPost(e) {
   const memberCreator = postData.action.memberCreator.username
   const actionType = postData.action.type
   const boardName = postData.action.data.board.name
-  const postObject = {}
-  postObject.actionId = postData.action.id
-  postObject.actionDate = new Date(postData.action.date)
-  postObject.cardId = postData.action.data.card.id
-  postObject.listId = postData.action.data.list.id
-  postObject.listName = postData.action.data.list.name
-  postObject.bill = accountingItem.reduce(function (bill, array) {
-    if (array.nomenclature == postData.action.data.card.name) {
-      bill = array.bill
-    }
-    return bill
-  })
-  postObject.account = accountingItem.reduce(function (account, array) {
-    if (array.nomenclature == postData.action.data.card.name) {
-      account = array.account
-    }
-    return account
-  })
-  postObject.nomenclature = postData.action.data.card.name
-  postObject.sum = parseComment(postData).sum
-  postObject.comment = parseComment(postData).comment
   var ss = SpreadsheetApp.openById(sourceSheetID).getSheetByName('test')
-  ss.getRange(1, 1).setValue(postObject)
+  ss.getRange(1, 1).setValue(postData)
   if (actionType == 'commentCard' && idMemberCreator !== '5e2b5f3f409c544ebdb1b9d4') {
+    const postObject = {}
+    postObject.actionId = postData.action.id
+    postObject.actionDate = new Date(postData.action.date)
+    postObject.cardId = postData.action.data.card.id
+    postObject.listId = postData.action.data.list.id
+    postObject.listName = postData.action.data.list.name
+    postObject.bill = accountingItem.reduce(function (bill, array) {
+      if (array.nomenclature == postData.action.data.card.name) {
+        bill = array.bill
+      }
+      return bill
+    })
+    postObject.account = accountingItem.reduce(function (account, array) {
+      if (array.nomenclature == postData.action.data.card.name) {
+        account = array.account
+      }
+      return account
+    })
+    postObject.nomenclature = postData.action.data.card.name
+    postObject.sum = parseComment(postData).sum
+    postObject.comment = parseComment(postData).comment
     if (boardName == targetSheetNameFact) {
       if (postObject.actionDate > maxDateFactTrelloBuffer) {
         updateTrelloBuffer(postObject, sourceSheetID, sourceSheetNameFactTrello)
