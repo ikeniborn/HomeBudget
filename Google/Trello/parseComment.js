77484f0cedcc35320e7739ff6ea762156399e42a
ikeniborn <ikeniborn@gmail.com>
2020-02-04 19:43:45 +0300

    up

--- Google/Trello/parseComment.js
+++ Google/Trello/parseComment.js
@@ -4,6 +4,8 @@ function parseComment(text) {
     parseData.mvz = 'BMW X5'
   } else if (text.toLowerCase().match('гараж')) {
     parseData.mvz = 'Гараж'
+  } else if (text.toLowerCase().match('квартира')) {
+    parseData.mvz = 'Квартира Котлярова'
   }
   parseData.sum = +text.match(/^\d+/)
   parseData.comment = text.split(parseData.sum).join('').replace(/^[.,\,, ,\-,\/,\\]/, ' ').trim()
