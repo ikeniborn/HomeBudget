66b741ba8dc78355000648a210151aea069e4824
ikeniborn <ikeniborn@gmail.com>
2020-02-07 22:02:54 +0300

    up

--- Google/Trello/updateFactPeriod.js
+++ Google/Trello/updateFactPeriod.js
@@ -5,13 +5,16 @@ function updateFactPeriod(postObject) {
     period.period = formatterDate(new Date(actionDate.getYear(), actionDate.getMonth(), 1))
     period.day = actionDate.getDate()
     updateParametr(sourceSheetID, parametrSheetName, 'periodFactIlya', period.period)
-    updateParametr(sourceSheetID, parametrSheetName, 'periodFactFamily', period.period)
     updateParametr(sourceSheetID, parametrSheetName, 'revenueDayIlya', period.day)
-    updateParametr(sourceSheetID, parametrSheetName, 'revenueDayFamily', period.day)
   } else if (['Оксана'].indexOf(postObject.cfo) !== -1) {
     period.period = formatterDate(new Date(actionDate.getYear(), actionDate.getMonth(), 1))
     period.day = actionDate.getDate()
     updateParametr(sourceSheetID, parametrSheetName, 'periodFactOksana', period.period)
     updateParametr(sourceSheetID, parametrSheetName, 'revenueDayOksana', period.day)
+  } else if (['Семья'].indexOf(postObject.cfo) !== -1) {
+    period.period = formatterDate(new Date(actionDate.getYear(), actionDate.getMonth(), 1))
+    period.day = actionDate.getDate()
+    updateParametr(sourceSheetID, parametrSheetName, 'periodFactFamily', period.period)
+    updateParametr(sourceSheetID, parametrSheetName, 'revenueDayFamily', period.day)
   }
 }
