d8de8e286f56ab5df67135e6e5e09be1867c3007
ikeniborn <ikeniborn@gmail.com>
2020-03-02 11:28:07 +0300

    Расчет исполнения

--- Google/Trello/updateBalanceCard.js
+++ Google/Trello/updateBalanceCard.js
@@ -10,10 +10,6 @@ function updateBalanceCard(postObject, sumData) {
   postObjectBalance.cardId = balanceCard.id
   var comment = '*Остаток бюджета:* ' + sumData.totalBudget + postObjectBalance.lineBreak
   comment += '*Остаток средств:* ' + sumData.totalFact + postObjectBalance.lineBreak
-  if (postObjectBalance.bill == 'Приход') {
-    comment += '*Исполнение по приходу:* ' + ((sumData.incomeFact / sumData.incomeBudget) * 100).toFixed(2) + encodeData('%', '%')
-  } else if ((postObjectBalance.bill == 'Расход')) {
-    comment += '*Исполнение по расходу:* ' + ((sumData.expenseFact / sumData.expenseBudget) * 100).toFixed(2) + encodeData('%', '%')
-  }
+  comment += '*Исполнение:* ' + (((sumData.incomeFact + sumData.expenseFact) / (sumData.incomeBudget + sumData.expenseBudget)) * 100).toFixed(2) + encodeData('%', '%')
   addCardComment(postObjectBalance, comment)
 }
