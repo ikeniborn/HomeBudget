75259e438049eb991bad4493e1ecdda58128a911
ikeniborn <ikeniborn@gmail.com>
2020-03-04 22:28:06 +0300

    обработка и проверка строки

--- Google/Trello/getCostСenter.js
+++ Google/Trello/getCostСenter.js
@@ -1,26 +1,31 @@
 function getCostСenter(postObject) {
   try {
     var array = postObject.costСenterArray
-    var text = postObject.text
-    var mvz = {}
-    mvz.item = {}
-    mvz.array = []
-    array.reduce(function (row, array) {
-      if (text.toLowerCase().match(array[2].toLowerCase())) {
-        row = {}
-        row.id = array[0]
-        row.mvz = array[1]
-        row.tag = array[2]
-        mvz.item = row
-        mvz.array.push(row)
-      } else {
-        row = {}
-        row.id = array[0]
-        row.mvz = array[1]
-        row.tag = array[2]
-        mvz.array.push(row)
-      }
-    }, {})
+    var text
+    if (Object.prototype.toString.call(postObject.comment) == '[object String]') {
+      text = postObject.comment
+      var mvz = {}
+      mvz.item = {}
+      mvz.array = []
+      array.reduce(function (row, array) {
+        if (text.toLowerCase().replace(/\s+/g, '').trim().match(array[2].toLowerCase())) {
+          row = {}
+          row.id = array[0]
+          row.mvz = array[1]
+          row.tag = array[2]
+          mvz.item = row
+          mvz.array.push(row)
+        } else {
+          row = {}
+          row.id = array[0]
+          row.mvz = array[1]
+          row.tag = array[2]
+          mvz.array.push(row)
+        }
+      }, {})
+    } else {
+      mvz = null
+    }
     return mvz
   } catch (e) {
     console.error('getCostСenter: ' + e)
