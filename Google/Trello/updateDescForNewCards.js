25a776c017e14d2c641b70e25607ca7972e6412b
ikeniborn <ikeniborn@gmail.com>
2020-03-13 16:12:24 +0300

    обновление

--- Google/Trello/updateDescForNewCards.js
+++ Google/Trello/updateDescForNewCards.js
@@ -1,14 +1,17 @@
 function updateDescForNewCards(postObject) {
   try {
     var list = getList(postObject, postObject.boardIdFact)
-    var cards = getCards(postObject, list.id)
+    var cards = getCards(postObject, list.id).array
     //* обновление описание карточки
     cards.forEach(function (card) {
       var postObjectCard = JSON.parse(JSON.stringify(postObject))
       postObjectCard.cardId = card.id
+      postObjectCard.cardName = card.name
+      postObjectCard.cardLabelColor = card.color
+      postObjectCard.accountingItem = getAccountingItem(postObjectCard)
+      postObjectCard.bill = postObjectCard.accountingItem.item.bill
+      postObjectCard.account = postObjectCard.accountingItem.item.account
       postObjectCard.nomenclature = card.name
-      postObjectCard.bill = card.bill
-      postObjectCard.account = card.account
       var description = getDescription(postObjectCard)
       if (description.haveBudget) {
         postObjectCard.cardDescription = description.text
