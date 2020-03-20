6c45a1d10d5b94571568b6658eba26005254c1b9
ikeniborn <ikeniborn@gmail.com>
2020-03-20 19:39:08 +0300

    логирование

---
+++ Google/Trello/addLog.js
@@ -0,0 +1,36 @@
+function addLog(postObject) {
+  var sourceOpen = postObject.logOpen
+  var endDate = new Date()
+  var startDate = new Date()
+  startDate.setDate(endDate.getDate() - 180)
+  var deleteArrya = []
+  var sourceArray = postObject.logArray.reduce(function (row, array, index) {
+    if (index > 0) {
+      if (array[0] >= startDate) {
+        row.push(array)
+      } else {
+        deleteArrya.push(index + 1)
+      }
+    }
+    return row
+  }, [])
+  if (deleteArrya.length > 0) {
+    var startDeleteIndex = deleteArrya.reduce(function (a, b) {
+      return a < b ? a : b
+    })
+    var countDeleteRow = deleteArrya.reduce(function (a, b) {
+      return a > b ? a : b
+    })
+    sourceOpen.deleteRows(startDeleteIndex, countDeleteRow)
+  }
+  var isNewAction = sourceArray.reduce(function (row, array) {
+    if (array[2] == postObject.actionId) {
+      row = false
+    }
+    return row
+  }, true)
+  if (isNewAction) {
+    sourceOpen.appendRow([postObject.webHookDate, postObject.actionType, postObject.actionId])
+  }
+  return isNewAction
+}
