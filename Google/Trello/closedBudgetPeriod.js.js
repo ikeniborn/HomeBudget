3dd2c20c72aa2cd09539368de0bcac657ad3b4d7
ikeniborn <ikeniborn@gmail.com>
2020-03-25 21:17:10 +0300

    693

--- Google/Trello/closedBudgetPeriod.js.js
+++ Google/Trello/closedBudgetPeriod.js.js
@@ -2,17 +2,19 @@
 /* eslint-disable spaced-comment */
 function closedBudgetPeriod(postObject) {
   try {
-    updateParametr(postObject, postObject.budgetPeriod2)
+
     const postObjectBudget = copyObject(postObject)
+    postObjectBudget.type = 'Бюджет'
+    updateParametr(postObject, postObject.budgetPeriod)
     postObjectBudget.boardId = postObjectBudget.boardIdBudget
     postObjectBudget.listId = getList(postObjectBudget).id
     postObjectBudget.listName = postObjectBudget.cfo + ' ' + formatterDate(postObjectBudget.budgetPeriod).date
-    const postObjectBudget2 = copyObject(postObject)
-    postObjectBudget2.boardId = postObjectBudget.boardIdBudget2
+    const postObjectBudget2 = copyObject(postObjectBudget)
+    postObjectBudget2.boardId = postObjectBudget2.boardIdBudget2
     postObjectBudget2.listId = getList(postObjectBudget2).id
     postObjectBudget2.listName = postObjectBudget2.cfo + ' ' + formatterDate(postObjectBudget2.budgetPeriod2).date
-    const postObjectBudget3 = copyObject(postObject)
-    postObjectBudget3.boardId = postObjectBudget.boardIdBudget3
+    const postObjectBudget3 = copyObject(postObjectBudget2)
+    postObjectBudget3.boardId = postObjectBudget3.boardIdBudget3
     postObjectBudget3.listId = getList(postObjectBudget3).id
     postObjectBudget3.listName = postObjectBudget3.cfo + ' ' + formatterDate(postObjectBudget3.budgetPeriod3).date
     var countCardsListBudget2 = getCards(postObjectBudget2).array
@@ -26,10 +28,11 @@ function closedBudgetPeriod(postObject) {
     }
     if (countCardsListBudget3.length == 0) {
       createCardsForList(postObjectBudget2)
+      createCardsForList(postObjectBudget3)
     } else {
       moveAllCards(postObjectBudget3, postObjectBudget2)
+      createCardsForList(postObjectBudget3)
     }
-    createCardsForList(postObjectBudget3)
     updateList(postObjectBudget)
     updateList(postObjectBudget2)
     updateList(postObjectBudget2)
