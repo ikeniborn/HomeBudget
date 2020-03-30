77c55ad3d74208f26ca60430f5eb7b110e58a135
ikeniborn <ikeniborn@gmail.com>
2020-03-30 16:09:23 +0300

    v701

--- Google/Trello/copyObject.js
+++ Google/Trello/copyObject.js
@@ -1,6 +1,16 @@
 function copyObject(object) {
   if (Object.prototype.toString.call(object) == '[object Object]') {
-    return JSON.parse(JSON.stringify(object))
+    var allObject = Object.keys(object)
+    var copyObject = {}
+    allObject.forEach(function (obj) {
+      const item = object[obj]
+      if (Object.prototype.toString.call(item) == '[object Date]') {
+        copyObject[obj] = new Date(JSON.parse(JSON.stringify(item)))
+      } else {
+        copyObject[obj] = JSON.parse(JSON.stringify(item))
+      }
+    })
+    return copyObject
   } else {
     return {}
   }
