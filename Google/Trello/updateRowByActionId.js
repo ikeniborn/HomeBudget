a5ee0320fd874b79c7e9593f7696e9ea902570ed
ikeniborn <ikeniborn@gmail.com>
2020-04-01 20:36:11 +0300

    v735 - ошибка

--- Google/Trello/updateRowByActionId.js
+++ Google/Trello/updateRowByActionId.js
@@ -17,8 +17,8 @@ function updateRowByActionId(postObject) {
     })
     sourceRows.forEach(function (row) {
       ss.getRange(row.indexRow, 1).setValue(postObject.actionDate)
-      ss.getRange(row.indexRow, 5).setValue(postObject.sum)
-      ss.getRange(row.indexRow, 6).setValue(postObject.comment)
+      ss.getRange(row.indexRow, 6).setValue(postObject.sum)
+      ss.getRange(row.indexRow, 7).setValue(postObject.comment)
     })
     //* обновление данных на листе учета
     ts = postObject.accountOpen
