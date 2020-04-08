e1f86081fd2730685ec10fefb2ce2d7149855c78
ikeniborn <ikeniborn@gmail.com>
2020-04-08 08:04:23 +0300

    up

--- _trelloDataProcessing.js
+++ _trelloDataProcessing.js
@@ -64,6 +64,16 @@ function addError(postObject) {
   }
 }
 
+function openGoogleSheet(sheetID, sheetName) {
+  try {
+    // открытие листа
+    return SpreadsheetApp.openById(sheetID).getSheetByName(sheetName)
+  } catch (e) {
+    postObject.error += arguments.callee.name + ': ' + e + postObject.lineBreakCell
+    addError(postObject)
+  }
+}
+
 function getPostObject(postData) {
   try {
     var postObject = getGlobalVariable()
@@ -457,101 +467,6 @@ function deleteRowByActionId(postObject) {
     //* получение данных учета после обновления
     postObject.dataAccount = getAllData(postObject, 'account')
     return sum
-  } catch (e) {
-    postObject.error += arguments.callee.name + ': ' + e + postObject.lineBreakCell
-    addError(postObject)
-  }
-}
-
-function doPost(e) {
-  try {
-    const parseAction = ['commentCard', 'updateComment', 'deleteComment', 'createList', 'updateList', 'updateCard']
-    const botUser = ['5e2b5f3f409c544ebdb1b9d4']
-    var postData = JSON.parse(e.postData.contents)
-    // if (['updateCard'].indexOf(postData.action.type) !== -1) {
-    //   let postObject = getPostObject(postData)
-    //   let errorOpen = openGoogleSheet(postObject.sourceSheetID, postObject.sourceSheetNameError)
-    //   errorOpen.appendRow([formatterDate().timestamp, postData.action.type, postData.action.id, '', '', '', postObject])
-    // }
-    if (parseAction.indexOf(postData.action.type) !== -1 && botUser.indexOf(postData.action.memberCreator.id) === -1 && addLog(postData)) {
-      var postObject = getPostObject(postData)
-      if (postObject.actionType == 'commentCard') {
-        //* добавление информации
-        updateTrelloData(postObject)
-        if (postObject.isTarget) {
-          //* обновление листа цель
-          updateTargetList(postObject)
-        }
-        //* получение описание карточки и комментария
-        postObject.cardDescription = getDescription(postObject).text
-        postObject.cardComment = getComment(postObject).text
-        //* обновление описание карточки
-        updateCardDesc(postObject)
-        //* обновление карточки баланса
-        updateBalanceCard(postObject)
-        //* добавление реакции на комментарий
-        addCardReaction(postObject)
-      } else if (postObject.actionType == 'updateComment' && postObject.isOldData) {
-        //* обновление данных при изменении комментария
-        updateRowByActionId(postObject)
-        if (postObject.isTarget) {
-          //* обновление листа цель
-          updateTargetList(postObject)
-        }
-        postObject.cardDescription = getDescription(postObject).text
-        postObject.cardComment = getComment(postObject).text
-        //* обновление описание карточки
-        updateCardDesc(postObject)
-        //* обновление карточки баланса
-        updateBalanceCard(postObject)
-        addCardReaction(postObject)
-      } else if (postObject.actionType == 'deleteComment' && postObject.isOldData) {
-        //* удаление строки при удалении комментария
-        postObject.sum = deleteRowByActionId(postObject)
-        if (postObject.isTarget) {
-          //* обновление листа цель
-          updateTargetList(postObject)
-        }
-        postObject.cardDescription = getDescription(postObject).text
-        postObject.cardComment = getComment(postObject).text
-        //* обновление описание карточки
-        updateCardDesc(postObject)
-        //* обновление карточки баланса
-        updateBalanceCard(postObject)
-      } else if (postObject.actionType == 'createList') {
-        if (postObject.isFact || postObject.isBudget) {
-          addFinancialCenter(postObject)
-          postObject.cfo = postObject.listName
-          if (postObject.isFact) {
-            postObject.type = 'Факт'
-          } else if (postObject.isBudget) {
-            postObject.type = 'Бюджет'
-          }
-          createCardsForList(postObject)
-          updateDescForNewCards(postObject)
-        } else if (postObject.isTarget) {
-          addTarget(postObject)
-          createCardsForList(postObject)
-        }
-      } else if (postObject.actionType == 'updateList') {
-        if (postObject.isTarget && postObject.listClosed) {
-          updateTarget(postObject)
-        }
-      } else if (postObject.actionType == 'updateCard') {
-        if (postObject.cardClosed && postObject.cardName == 'Баланс') {
-          if (postObject.isCurrFact) {
-            //* закрытие фактического периода
-            updateParametr(postObject)
-            closedFactPeriod(postObject)
-            updateDescForNewCards(postObject)
-          } else if (postObject.isCurrBudget) {
-            //* закрытие бюджетного периода
-            updateParametr(postObject)
-            closedBudgetPeriod(postObject)
-          }
-        }
-      }
-    }
   } catch (e) {
     postObject.error += arguments.callee.name + ': ' + e + postObject.lineBreakCell
     addError(postObject)
@@ -1644,10 +1559,95 @@ function getPreviousFact(postObject) {
   }
 }
 
-function openGoogleSheet(sheetID, sheetName) {
+function doPost(e) {
   try {
-    // открытие листа
-    return SpreadsheetApp.openById(sheetID).getSheetByName(sheetName)
+    const parseAction = ['commentCard', 'updateComment', 'deleteComment', 'createList', 'updateList', 'updateCard']
+    const botUser = ['5e2b5f3f409c544ebdb1b9d4']
+    var postData = JSON.parse(e.postData.contents)
+    // if (['updateCard'].indexOf(postData.action.type) !== -1) {
+    //   let postObject = getPostObject(postData)
+    //   let errorOpen = openGoogleSheet(postObject.sourceSheetID, postObject.sourceSheetNameError)
+    //   errorOpen.appendRow([formatterDate().timestamp, postData.action.type, postData.action.id, '', '', '', postObject])
+    // }
+    if (parseAction.indexOf(postData.action.type) !== -1 && botUser.indexOf(postData.action.memberCreator.id) === -1 && addLog(postData)) {
+      var postObject = getPostObject(postData)
+      if (postObject.actionType == 'commentCard') {
+        //* добавление информации
+        updateTrelloData(postObject)
+        if (postObject.isTarget) {
+          //* обновление листа цель
+          updateTargetList(postObject)
+        }
+        //* получение описание карточки и комментария
+        postObject.cardDescription = getDescription(postObject).text
+        postObject.cardComment = getComment(postObject).text
+        //* обновление описание карточки
+        updateCardDesc(postObject)
+        //* обновление карточки баланса
+        updateBalanceCard(postObject)
+        //* добавление реакции на комментарий
+        addCardReaction(postObject)
+      } else if (postObject.actionType == 'updateComment' && postObject.isOldData) {
+        //* обновление данных при изменении комментария
+        updateRowByActionId(postObject)
+        if (postObject.isTarget) {
+          //* обновление листа цель
+          updateTargetList(postObject)
+        }
+        postObject.cardDescription = getDescription(postObject).text
+        postObject.cardComment = getComment(postObject).text
+        //* обновление описание карточки
+        updateCardDesc(postObject)
+        //* обновление карточки баланса
+        updateBalanceCard(postObject)
+        addCardReaction(postObject)
+      } else if (postObject.actionType == 'deleteComment' && postObject.isOldData) {
+        //* удаление строки при удалении комментария
+        postObject.sum = deleteRowByActionId(postObject)
+        if (postObject.isTarget) {
+          //* обновление листа цель
+          updateTargetList(postObject)
+        }
+        postObject.cardDescription = getDescription(postObject).text
+        postObject.cardComment = getComment(postObject).text
+        //* обновление описание карточки
+        updateCardDesc(postObject)
+        //* обновление карточки баланса
+        updateBalanceCard(postObject)
+      } else if (postObject.actionType == 'createList') {
+        if (postObject.isFact || postObject.isBudget) {
+          addFinancialCenter(postObject)
+          postObject.cfo = postObject.listName
+          if (postObject.isFact) {
+            postObject.type = 'Факт'
+          } else if (postObject.isBudget) {
+            postObject.type = 'Бюджет'
+          }
+          createCardsForList(postObject)
+          updateDescForNewCards(postObject)
+        } else if (postObject.isTarget) {
+          addTarget(postObject)
+          createCardsForList(postObject)
+        }
+      } else if (postObject.actionType == 'updateList') {
+        if (postObject.isTarget && postObject.listClosed) {
+          updateTarget(postObject)
+        }
+      } else if (postObject.actionType == 'updateCard') {
+        if (postObject.cardClosed && postObject.cardName == 'Баланс') {
+          if (postObject.isCurrFact) {
+            //* закрытие фактического периода
+            updateParametr(postObject)
+            closedFactPeriod(postObject)
+            updateDescForNewCards(postObject)
+          } else if (postObject.isCurrBudget) {
+            //* закрытие бюджетного периода
+            updateParametr(postObject)
+            closedBudgetPeriod(postObject)
+          }
+        }
+      }
+    }
   } catch (e) {
     postObject.error += arguments.callee.name + ': ' + e + postObject.lineBreakCell
     addError(postObject)
