8b485a8328c81d0f93254e4b1a5df0e3b5c021d9
ikeniborn <ikeniborn@gmail.com>
2020-05-07 19:22:56 +0300

    up

--- _trelloDataProcessing.js
+++ _trelloDataProcessing.js
@@ -379,6 +379,38 @@ function getPostObject(postData) {
 }
 
 // обновление параметра
+function addFinancialCenter(postObject) {
+  /*
+   * @postObject - входные параметра запроса
+   * */
+  try {
+    var ss = postObject.financialCenterSheetOpen
+    var ssParametr = postObject.parametrSheetOpen
+    var array = getFinancialСenter(postObject).array
+    var arrayPаrametr = getParametr(postObject).array
+    var cfoArray = array.map(function (array) {
+      return array.id
+    })
+    var parametrArray = arrayPаrametr.map(function (array) {
+      return array.id
+    })
+    if (postObject.cfo == undefined) {
+      var newId = cfoArray.length + 1
+      ss.appendRow([newId, postObject.listName, formatterDate().timestamp])
+      postObject.financialСenterArray = getGoogleSheetValues(postObject.financialCenterSheetOpen)
+      var newIdParametr = parametrArray.length + 1
+      ssParametr.appendRow([newIdParametr, 'Факт', postObject.listName, postObject.factPeriod, formatterDate().timestamp])
+      ssParametr.appendRow([newIdParametr + 1, 'Бюджет', postObject.listName, postObject.budgetPeriod, formatterDate().timestamp])
+    }
+    //* обновление листа
+    postObject.listName = postObject.listName + ' ' + formatterDate(postObject.period).date
+    updateList(postObject)
+  } catch (e) {
+    postObject.error.push(arguments.callee.name + ': ' + e)
+  }
+}
+
+// обновление параметра
 function addTarget(postObject) {
   /*
    * @postObject - входные параметра запроса
