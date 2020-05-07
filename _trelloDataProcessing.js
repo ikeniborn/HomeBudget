4c5e1701d3493ed0ac356b7dace20f1ba7d41919
ikeniborn <ikeniborn@gmail.com>
2020-05-07 22:59:03 +0300

    up

--- _trelloDataProcessing.js
+++ _trelloDataProcessing.js
@@ -1556,6 +1556,7 @@ function isValidateAction(postData) {
 function doPost(e) {
   try {
     const postData = JSON.parse(e.postData.contents)
+    addErrorItem(arguments.callee.name + ': ' + objectToString(postData))
     if (addLog(postData)) {
       var postObject = getPostObject(postData)
       if (isMatch(postObject.actionType, 'commentCard')) {
