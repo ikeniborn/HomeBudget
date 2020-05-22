f7051b79e82c5eb1a6f6289337f8ad5c7879c5ca
ikeniborn <ikeniborn@gmail.com>
2020-05-22 10:38:16 +0300

    up

--- _trelloDataProcessing.js
+++ _trelloDataProcessing.js
@@ -914,6 +914,11 @@ function getPeriod(postObject) {
       date.factPeriod2 = new Date(date.factPeriod.getFullYear(), date.factPeriod.getMonth() - 2, 1)
       date.budgetPeriod2 = new Date(date.budgetPeriod.getFullYear(), date.budgetPeriod.getMonth() + 1, 1)
       date.budgetPeriod3 = new Date(date.budgetPeriod.getFullYear(), date.budgetPeriod.getMonth() + 2, 1)
+      if (getYMD(date.factPeriod).ymd === getYMD(date.budgetPeriod).ymd) {
+        date.budgetPeriodCurrent = date.budgetPeriod
+      } else {
+        date.budgetPeriodCurrent = date.factPeriod
+      }
     } else if (postObject.isBudget) {
       const postObjectCopy = copyObject(postObject)
       postObjectCopy.type = 'Факт'
@@ -923,11 +928,7 @@ function getPeriod(postObject) {
       date.factPeriod2 = new Date(date.factPeriod.getFullYear(), date.factPeriod.getMonth() - 2, 1)
       date.budgetPeriod2 = new Date(date.budgetPeriod.getFullYear(), date.budgetPeriod.getMonth() + 1, 1)
       date.budgetPeriod3 = new Date(date.budgetPeriod.getFullYear(), date.budgetPeriod.getMonth() + 2, 1)
-    }
-    if (getYMD(date.factPeriod).ymd === getYMD(date.budgetPeriod).ymd) {
       date.budgetPeriodCurrent = date.budgetPeriod
-    } else {
-      date.budgetPeriodCurrent = date.factPeriod
     }
     if (isMatch(postObject.boardId, postObject.boardIdFact)) {
       date.period = date.factPeriod
